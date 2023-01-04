646b2a5fca8b3110317f7b6781e66349a9baa88b
kennedyisn <kennedyhayles@gmail.com>
2023-01-04 15:51:21 -0500

    Final bug fixes to get local storage saved

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ var today = moment();
 // This line connects my code to the "currentDay"ID on line 30 and reformats is
 $("#currentDay").text(today.format('MMMM Do YYYY, h:mm:ss a'));
 
-var nineamTodo = $('input[name="9AM"]')
-var tenamTodo = $('input[name="10AM"]')
-var elevenamTodo = $('input[name="11AM"]')
-var twelvepmTodo = $('input[name="12PM"]')
-var onepmTodo = $('input[name="1PM"]')
-var twopmTodo = $('input[name="2PM"]')
-var threepmTodo = $('input[name="3PM"]')
-var fourpmTodo = $('input[name="4PM"]')
-var fivepmTodo = $('input[name="5PM"]')
+var nineamTodo = $('textarea[name="9AM"]')
+var tenamTodo = $('textarea[name="10AM"]')
+var elevenamTodo = $('textarea[name="11AM"]')
+var twelvepmTodo = $('textarea[name="12PM"]')
+var onepmTodo = $('textarea[name="1PM"]')
+var twopmTodo = $('textarea[name="2PM"]')
+var threepmTodo = $('textarea[name="3PM"]')
+var fourpmTodo = $('textarea[name="4PM"]')
+var fivepmTodo = $('textarea[name="5PM"]')
 
 function savedTodoInfo(event) {
   // Prevent the default behavior
@@ -27,23 +27,24 @@ function savedTodoInfo(event) {
   console.log('At 4 PM I have to: ', fourpmTodo.val());
   console.log('At 5 PM I have to: ', fivepmTodo.val());
 
-  var todoInfo = $('input:text')
+  var todoInfo = $('textarea')
   // Loop through checked options to store in array
-  $.each(todoInfo, function () {
-    localStorage.setItem($(this).val());
+  $('textarea').each(function () {
+    localStorage.setItem($(this).attr("name"),$(this).val());
   });
 }
 
+
 //Code to pull our previously saved information from the local storage
-$("#9AM .description").val(localStorage.getItem("9AM"));
-$("#10AM .description").val(localStorage.getItem("10AM"));
-$("#11AM .description").val(localStorage.getItem("11AM"));
-$("#12PM .description").val(localStorage.getItem("12PM"));
-$("#1PM .description").val(localStorage.getItem("1PM"));
-$("#2PM .description").val(localStorage.getItem("2PM"));
-$("#3PM .description").val(localStorage.getItem("3PM"));
-$("#4PM .description").val(localStorage.getItem("4PM"));
-$("#5PM .description").val(localStorage.getItem("5PM"));
+$('textarea[name="9AM"]').val(localStorage.getItem("9AM"));
+$('textarea[name="10AM"]').val(localStorage.getItem("10AM"));
+$('textarea[name="11AM"]').val(localStorage.getItem("11AM"));
+$('textarea[name="12PM"]').val(localStorage.getItem("12PM"));
+$('textarea[name="1PM"]').val(localStorage.getItem("1PM"));
+$('textarea[name="2PM"]').val(localStorage.getItem("2PM"));
+$('textarea[name="3PM"]').val(localStorage.getItem("3PM"));
+$('textarea[name="4PM"]').val(localStorage.getItem("4PM"));
+$('textarea[name="5PM"]').val(localStorage.getItem("5PM"));
 
 //Creates function that will turn each timeblock id from a string into a number
 $('.hour').each(function() {
@@ -73,3 +74,5 @@ $('.hour').each(function() {
     $(this).removeClass('future');
   }
 });
+
+$('.fa-save').on("click",savedTodoInfo)
